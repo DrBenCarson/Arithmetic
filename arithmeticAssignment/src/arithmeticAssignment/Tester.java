d40055032e9006f8f365869176717c0dd18a7f09
DrBenCarson <adrianoz@comcast.net>
2016-09-13 11:06:16 -0600

    Added main

---
+++ arithmeticAssignment/src/arithmeticAssignment/Tester.java
@@ -0,0 +1,17 @@
+package arithmeticAssignment;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("79+3*(4+82-68)-7+19=");
+		System.out.print(79+3*(4+82-68)-7+19);
+		System.out.println();
+		System.out.print("(179+21+10)/7+181=");
+		System.out.print((179+21+10)/7+181);
+		System.out.println();
+		System.out.print("10389*56*11+2246=");
+		System.out.print(10389*56*11+2246);
+	}
+
+}
